80edf57d22f998ab2a8e56c857c652b46af12594
MichaelB167 <michael.m.bates@gmail.com>
2016-05-27 23:26:13 -0400

    Create join tables for skills and jobs

--- scripts/create/jobs.sql
+++ scripts/create/jobs.sql
@@ -1,1 +1,9 @@
 -- create jobs connecting people and companies
+CREATE TABLE jobs(
+  id SERIAL PRIMARY KEY,
+  note CHARACTER VARYING,
+  start_date DATE,
+  end_date DATE,
+  people_id INTEGER REFERENCES people,
+  companies_id INTEGER REFERENCES companies
+);

a4f412278f5bb235b67475f585e593df7f1b2c60
MichaelB167 <michael.m.bates@gmail.com>
2016-05-27 23:26:37 -0400

    Create tables for addresses, cities, companies, endorsements, people

--- scripts/create/companies.sql
+++ scripts/create/companies.sql
@@ -1,1 +1,8 @@
 -- create a table for companies
+CREATE TABLE companies(
+  id SERIAL PRIMARY KEY,
+  name CHARACTER VARYING,
+  city CHARACTER VARYING,
+  region CHARACTER VARYING,
+  country CHARACTER VARYING
+);
--- scripts/create/endorsements.sql
+++ scripts/create/endorsements.sql
@@ -1,1 +1,7 @@
 -- create endorsements connecting people and skills
+CREATE TABLE endorsements(
+  id SERIAL PRIMARY KEY,
+  note CHARACTER VARYING,
+  people_id INTEGER REFERENCES people,
+  skills_id INTEGER REFERENCES skills
+);

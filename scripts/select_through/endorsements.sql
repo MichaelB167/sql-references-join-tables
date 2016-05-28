7fbfb65544839825e1cd436af384198ebc00e59b
MichaelB167 <michael.m.bates@gmail.com>
2016-05-27 23:27:54 -0400

    add select-through joins for people and skills

--- scripts/select_through/endorsements.sql
+++ scripts/select_through/endorsements.sql
@@ -1,1 +1,12 @@
 -- get information using endorsements join table
+select p.given_name, p.surname
+  from people p
+  inner join endorsements e
+on p.id = e.id
+;
+
+select s.framework, s.language
+  from skills s
+  inner join endorsements e
+on s.id = e.skills_id
+;

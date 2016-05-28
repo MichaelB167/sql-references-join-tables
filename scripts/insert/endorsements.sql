2885a08002a3af390d94b2150273059c40931451
MichaelB167 <michael.m.bates@gmail.com>
2016-05-27 23:27:20 -0400

    Insert sample values into jobs, endorsements

--- scripts/insert/endorsements.sql
+++ scripts/insert/endorsements.sql
@@ -1,1 +1,6 @@
 -- insert endorsements connecting people and skills
+INSERT INTO endorsements(note,people_id,skills_id)
+VALUES ('Coolness', 1, 5);
+
+INSERT INTO endorsements(note,people_id,skills_id)
+VALUES ('Oldness', 2, 4);
--- scripts/insert/jobs.sql
+++ scripts/insert/jobs.sql
@@ -1,1 +1,3 @@
 -- insert jobs connecting people and companies
+INSERT INTO endorsements(note,start_date,end_date,people_id,companies_id)
+VALUES ('Coolness', 1, 5);
